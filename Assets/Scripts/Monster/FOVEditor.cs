ed7fdcf1b59dc2ba81290f1545539655c15a3f4b
박창혁 <bkh5125@gmail.com>
2023-08-16 17:07:27 +0900

    0816
    monster관련 스크립트 추가
    몬스터 프리팹 4개 추가
    레이어, 태그 추가
    - 패키지 -
    Polygonal - Creatures Pack v1.0
    Stylized Boar Boss - RPG Forest Animal v1.0
    Stylized Spider - RPG Forest Animal v1.0
    Stylized Stag Boss - RPG Forest Animal v1.0
    패키지에서 몬스터 모델 및 애니메이션 사용

---
+++ Assets/Scripts/Monster/FOVEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+//EnemyFOV 스크립트를 활용하기위한 커스텀에디터임을 명시
+[CustomEditor(typeof(Monster))]
+public class FOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        Monster fov = (Monster)target;
+        //시야각(원주) 시작점의 좌표를 계산(주어진 각도의 1/2 지점부터)
+        Vector3 fromAnglePos = fov.CirclePoint(-fov.viewAngle * 0.5f);
+
+        Handles.color = Color.white;
+        //원점좌표, 노멀벡터, 원의 반지름 사이즈
+        Handles.DrawWireDisc(fov.transform.position, Vector3.up, fov.viewRange);
+
+        Handles.color = new Color(1, 1, 1, 0.2f);
+        //SolidArd = 부채꼴
+        //원점좌표, 노멀벡터, 부채꼴 시작 각도, 부채꼴 각도, 부채꼴 반지름
+        Handles.DrawSolidArc(fov.transform.position, Vector3.up, fromAnglePos, fov.viewAngle, fov.viewRange);
+
+        Handles.Label(fov.transform.position + (fov.transform.forward * 2f), fov.viewAngle.ToString());
+    }
+}
