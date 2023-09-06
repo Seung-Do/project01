b43f969ed84253895df5785c6506d5d76c56a88e
Seung-Do <gablin21@hotmail.com>
2023-09-06 20:42:34 +0900

    0906_home
    ppp

--- Assets/Scripts/CheckPoint01.cs
+++ Assets/Scripts/CheckPoint01.cs
@@ -8,37 +8,64 @@ public class CheckPoint01 : MonoBehaviour
     [SerializeField] private GameObject location01;
     [SerializeField] private GameObject location02;
     [SerializeField] private GameObject checkPoint02;
-    [SerializeField] private GameObject[] monsters;
+    [SerializeField] private GameObject monsters;
     [SerializeField] private float playerDistance;
-
+    private float dist;
     void Start()
     {
+        StartCoroutine(Distance());
         StartCoroutine(Check());
     }
     void Update()
     {
-        
+
     }
+    /* IEnumerator Check()
+     {
+         while (true)
+         {
+             Collider[] colliders = Physics.OverlapSphere(transform.position, playerDistance);
+             foreach (Collider collider in colliders)
+             {
+                 if (collider.gameObject.layer == LayerMask.NameToLayer("PLAYER"))
+                 {
+                     location01.SetActive(false);
+                     location02.SetActive(true);
+                     checkPoint02.SetActive(true);
+                     foreach (GameObject monster in monsters)
+                     {
+                         monster.SetActive(true);
+                     }
+                     gameObject.SetActive(false);
+                 }
+             }
+             yield return new WaitForSeconds(1);
+         }
+     }*/
     IEnumerator Check()
     {
         while (true)
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, playerDistance);
-            foreach (Collider collider in colliders)
+            if (dist <= playerDistance)
             {
-                if (collider.gameObject.layer == LayerMask.NameToLayer("PLAYER"))
-                {
-                    location01.SetActive(false);
-                    location02.SetActive(true);
-                    checkPoint02.SetActive(true);
-                    foreach (GameObject monster in monsters)
-                    {
-                        monster.SetActive(true);
-                    }
-                    gameObject.SetActive(false);
-                }
+                location01.SetActive(false);
+                location02.SetActive(true);
+                
+                monsters.SetActive(true);
+                
+                checkPoint02.SetActive(true);
+                gameObject.SetActive(false);
             }
             yield return new WaitForSeconds(1);
+        }
+    }
+
+    IEnumerator Distance()
+    {
+        while (true)
+        {
+            dist = Vector3.Distance(GameManager.Instance.playerTr.position, transform.position);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
