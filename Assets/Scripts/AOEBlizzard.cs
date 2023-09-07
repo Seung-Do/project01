e23237308884cf4c3f75812066ae68b303dcac02
Seung-Do <gablin21@hotmail.com>
2023-09-07 21:34:37 +0900

    0907_home
    heo

--- Assets/Scripts/AOEBlizzard.cs
+++ Assets/Scripts/AOEBlizzard.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AOEBlizzard : MonoBehaviour
 {
@@ -25,35 +26,49 @@ public class AOEBlizzard : MonoBehaviour
             if (collider.gameObject.layer == LayerMask.NameToLayer("ENEMY") || collider.gameObject.layer == LayerMask.NameToLayer("BOSS"))
             {
                 Animator animator = collider.gameObject.GetComponent<Animator>();
-                StartCoroutine(ChangeAnimatorSpeed(animator,2f));   
+                NavMeshAgent agent = collider.GetComponent<NavMeshAgent>();
+                StartCoroutine(SlowDownAnimation(animator));
+                StartCoroutine(SlowDownAgent(agent));
                 IDamage damage = collider.gameObject.GetComponent<IDamage>();
                 if (damage != null)
                 {
-                    StartCoroutine(DamageWait(damage, wait));
+                    StartCoroutine(DamageWait(damage));
                 }
             }
         }
     }
-    IEnumerator DamageWait(IDamage damage, WaitForSeconds wait)
+    IEnumerator DamageWait(IDamage damage)
     {
         yield return wait;
         damage.getDamage(100);
     }
-    private IEnumerator ChangeAnimatorSpeed(Animator animator, float duration)
+    /* private IEnumerator ChangeAnimatorSpeed(Animator animator, float duration)
+     {
+         float elapsedTime = 0;
+         while (elapsedTime < duration)
+         {
+             float t = elapsedTime / duration;
+
+             float newSpeed = Mathf.Lerp(0.2f, 0.7f, t);
+             animator.speed = newSpeed;
+
+             elapsedTime += Time.deltaTime;
+             yield return null;
+         }
+
+         animator.speed = 1f; 
+     }*/
+    private IEnumerator SlowDownAnimation(Animator animator)
     {
-        float elapsedTime = 0;
-        while (elapsedTime < duration)
-        {
-            float t = elapsedTime / duration;
-
-            float newSpeed = Mathf.Lerp(0.2f, 0.7f, t);
-            animator.speed = newSpeed;
-
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        animator.speed = 1f; 
+        animator.speed = 0.5f;
+        yield return wait;
+        animator.speed = 1f;
+    }
+    private IEnumerator SlowDownAgent(NavMeshAgent agent)
+    {
+        agent.speed /= 2f;
+        yield return wait;
+        agent.speed *= 2f;
     }
 
 }
