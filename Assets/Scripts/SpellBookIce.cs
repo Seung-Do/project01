47944f7495f6f2a53b951df1b4425fe18ec56a1a
Seung-Do <gablin21@hotmail.com>
2023-09-12 22:05:48 +0900

    0912_hhh
    ppp

--- Assets/Scripts/SpellBookIce.cs
+++ Assets/Scripts/SpellBookIce.cs
@@ -7,6 +7,7 @@ public class SpellBookIce : MonoBehaviour
     private float rotationSpeed = 60f;
     [SerializeField] GameObject currTarget;
     [SerializeField] GameObject nextTarget;
+    [SerializeField] GameObject canvas;
     void Start()
     {
 
@@ -24,6 +25,8 @@ public class SpellBookIce : MonoBehaviour
             ControllManager controllManager = other.gameObject.GetComponent<ControllManager>();
             controllManager.bookSpell[2].SetActive(true);
             controllManager.icePosible = true;
+
+            canvas.SetActive(true);
             currTarget.SetActive(false);
             nextTarget.SetActive(true);
             gameObject.SetActive(false);
